387716ec48c47d77e123c813454ddccf94c31628
Kamil Neczaj <kneczaj@gmail.com>
2025-08-31 20:34:51 +0200

    feat: validate if vcs is installed

--- __install.sh
+++ __install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# Check if 'code' command exists
+if ! command -v code &> /dev/null; then
+    echo "Install Visual Studio code before. Skipping."
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 "${SCRIPT_DIR}/config/__install.sh"
 
